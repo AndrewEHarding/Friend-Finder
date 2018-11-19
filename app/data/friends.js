f74a2d17b4882494164f1a5337207e7cea649c66
Andrew Harding <dreweharding@gmail.com>
2018-11-19 14:20:26 -0500

    package.json up to date

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendList = [
+    {
+        name: "Bruce",
+        img: "https://media1.popsugar-assets.com/files/thumbor/hcgnOSifwbc5Y4vB8hFA8c_Ob8E/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2015/05/01/811/n/1922283/238890d5f4263f9f_Avengers-Age-Ultron-Pictures-3/i/Out-Bruce-Banner-Hulk.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Tony",
+        img: "https://observatoriodocinema.bol.uol.com.br/wp-content/uploads/2018/07/sort-Tony-Stark-Iron-Man.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Steve",
+        img: "https://pixel.nymag.com/imgs/daily/vulture/2016/08/25/25-captain-america-chris-evans.w330.h330.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    }
+];
+
+module.exports = friendList;
